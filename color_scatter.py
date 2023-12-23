0ef21da5880b8b6d4611ab67b8edb0a11af8b9c6
Lizhiq <56099052+lizhiq16@users.noreply.github.com>
2023-12-24 03:38:38 +0800

    Update color_scatter.py

--- color_scatter.py
+++ color_scatter.py
@@ -24,7 +24,7 @@ sc = plt.scatter(data1, data2, c=data3, cmap=cmap, vmin=xmin, vmax=abs(xmin), s=
 # 设置坐标轴范围和标题
 plt.xlim(xmin, xmax)
 plt.ylim(0, 2.5)
-plt.xlabel('sign(λ2)ρ', fontsize=16)
+plt.xlabel(r'sign($\lambda_2$)$\rho$', fontsize=16)
 plt.ylabel('IRI(a.u.)', fontsize=16)
 
 # 设置刻度文字大小
